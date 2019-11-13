cc2e5ad715cf2e0f15e848d45947b3929c570798
Fede <dfedericovera@gmail.com>
2019-11-13 00:13:57 -0300

    index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './componentes/login/login.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 
 const routes: Routes = [
-  { path: '',  component: PrincipalComponent, pathMatch: 'full' },
+  { path: '',  component: PrincipalComponent},
   { path: 'Login', component: LoginComponent },
   { path: 'Registro', component: RegistroComponent }
 ];
